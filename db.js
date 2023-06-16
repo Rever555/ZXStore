1bbdba149147ecf138cb75a122e50690dbd06e75
Rever555 <128627521+Rever555@users.noreply.github.com>
2023-06-17 01:30:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    password: "root",
-    host: "localhost",
+    user: "da",
+    password: "6BfJg1n3YtxfJHxDvenSgXhurdNn8GV9",
+    host: "dpg-ci6dr5tph6ekv7u7qdr0-a",
     port: 5432,
     database: "zxstore"
 })
